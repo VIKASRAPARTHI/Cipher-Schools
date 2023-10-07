087c31d84eba3d090612c07614ee357d88172a57
Raparthi Vikas <115341123+VIKASRAPARTHI@users.noreply.github.com>
2023-10-07 18:35:37 +0530

    Add files via upload

---
+++ Lecture-2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>     //this is a header
+using namespace std;        //std makes sure that cout is a part of "iostream"
+
+int main() {
+    cout<<"Hello World"<<endl;      //Output stream, which is displayed on the terminal
+    int num;
+    cin>>num;       //Input Stream, which takes input from the user
+    
+    return 0;
+}
