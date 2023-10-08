95aef9e74f26a577cc1df39da7725bcee8d27087
Raparthi Vikas <115341123+VIKASRAPARTHI@users.noreply.github.com>
2023-10-08 22:29:45 +0530

    Add files via upload

---
+++ Lecture3_2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Declare 2 variables and then assign them values of 3, 5 and then add them
+    int a = 3; 
+    int b = 5;
+
+    cout<<"a + b = "<<a+b<<endl;
+    
+    return 0;
+}
