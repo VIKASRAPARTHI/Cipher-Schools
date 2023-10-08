d3f13c9d55f3f0d17db3e2309cc734c9a1d43bf0
Raparthi Vikas <115341123+VIKASRAPARTHI@users.noreply.github.com>
2023-10-08 19:33:13 +0530

    Add files via upload

---
+++ Lecture-3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;        // Initialisation of variable
+    cin>>num;       //Input Stream
+    cout<<"The entered number is: "<<num<<endl;     //Ouput Stream
+
+    return 0;
+} 
+
+
+/* What are variables and constants?
+A constant does not change its value over time.
+ A variable, on the other hand, changes its value dependent on the equation. 
+ Constants are usually written in numbers. Variables are specially written in letters or symbols.
+  Constants usually represent the known values in an equation, expression or in line of programming*/
+
+/*how will the computer identify the kind of input we give in c++ ?
+The cin object in C++ is used to accept the input from the standard input device i.e., keyboard. 
+it is the instance of the class istream. It is associated with the standard C input stream stdin. 
+The extraction operator(>>) is used along with the object cin for reading inputs. 
+The extraction operator extracts the data from the object cin which is entered using the keyboard*/
+
+/*how will the computer store the input we give ?
+When you enter data into a computer, it is temporarily stored in the computer's Random Access Memory (RAM). 
+RAM is a type of volatile memory that provides fast and temporary storage for data that the computer is actively using*/
