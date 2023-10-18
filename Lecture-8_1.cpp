b7990a5adc7323a31b23fcc3f307de2e5deddaa2
Raparthi Vikas <115341123+VIKASRAPARTHI@users.noreply.github.com>
2023-10-18 22:11:47 +0530

    Add files via upload

---
+++ Lecture-8_1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+void printstuff() {         //there is no parameters
+    cout<<"Writing my first function"<<endl;
+}
+
+int main() {
+   printstuff();
+}
