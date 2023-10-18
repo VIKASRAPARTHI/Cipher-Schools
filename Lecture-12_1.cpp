56a3d11b3ce4556a352389c3ac17ebda756136b4
Raparthi Vikas <115341123+VIKASRAPARTHI@users.noreply.github.com>
2023-10-19 00:41:42 +0530

    Add files via upload

---
+++ Lecture-12_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //pointers
+    //Elements that can store the addresses of another variable
+
+    int a;
+    float b;
+    cout<<"The address of a is "<<&a;
+    cout<<"The address of b is "<<&b;
+    return 0;
+
+}
