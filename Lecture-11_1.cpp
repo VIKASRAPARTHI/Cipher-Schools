19da28c01d9f9afae7caaba7944724ec47cbbadf
Raparthi Vikas <115341123+VIKASRAPARTHI@users.noreply.github.com>
2023-10-19 00:01:30 +0530

    Add files via upload

---
+++ Lecture-11_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long a;
+    int b;
+
+    cout<<"The size of int is: "<<sizeof(b)<<" ,size of long long int is: "<<sizeof(a)<<endl;
+
+    return 0;
+}
